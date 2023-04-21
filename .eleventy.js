15f51104e06c380638273466a6a96e15d7296c66
Mátyás Budavári <budavariam@gmail.com>
2023-04-21 11:29:35 +0200

    Update code

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const csvParse = require("csv-parse/sync");
 const lunr = require("lunr");
+const path = require("path");
 const cheerio = require("cheerio");
 
 const { DateTime } = require("luxon");
@@ -12,6 +13,31 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { minify } = require("terser");
 const { cache } = require('eleventy-plugin-workbox');
+
+function splitHTMLIntoSections(htmlContent) {
+  const $ = cheerio.load(htmlContent);
+  const sections = [];
+  $('h1, h2, h3, h4, h5, h6').each((i, el) => {
+    const tagName = el.tagName.toLowerCase();
+    const $el = $(el);
+    const title = $el.text();
+    const content = [];
+    // Add all following sibling elements until the next header tag
+    let $next = $el.next();
+    while ($next.length && !$next.is(`h1, h2, h3, h4, h5, h6`)) {
+      content.push($next.html());
+      $next = $next.next();
+    }
+
+    sections.push({
+      tag: tagName,
+      id: $el.attr('id'),
+      title,
+      content: content.join(" "),
+    });
+  });
+  return sections
+}
 
 module.exports = function (eleventyConfig) {
   // Copy the `img` and `css` folders to the output
@@ -109,34 +135,56 @@ module.exports = function (eleventyConfig) {
     });
   });
 
-  eleventyConfig.addJavaScriptFunction("useLunr", function (cb) {
-    return lunr(cb)
-  });
+  // eleventyConfig.addJavaScriptFunction("useLunr", function (cb) {
+  //   return lunr(cb)
+  // });
+  eleventyConfig.addJavaScriptFunction("useLunr", function (data) {
+    // const lunrRegisterPipelineFn = this.lunrRegisterPipelineFn //defined in .eleventy.js
+    // const splitHTMLIntoSections = this.splitHTMLIntoSections //defined in .eleventy.js
+    // const cheerioLoad = this.useCheerioLoad //defined in .eleventy.js
 
-  eleventyConfig.addJavaScriptFunction("splitHTMLIntoSections", function (htmlContent) {
-    const $ = cheerio.load(htmlContent);
-    const sections = [];
-    $('h1, h2, h3, h4, h5, h6').each((i, el) => {
-      const tagName = el.tagName.toLowerCase();
-      const $el = $(el);
-      const title = $el.text();
-      const content = [];
-      // Add all following sibling elements until the next header tag
-      let $next = $el.next();
-      while ($next.length && !$next.is(`h1, h2, h3, h4, h5, h6`)) {
-        content.push($next.html());
-        $next = $next.next();
-      }
-
-      sections.push({
-        tag: tagName,
-        id: $el.attr('id'),
-        title,
-        content: content.join(" "),
-      });
+    // const removeHtmlTags = lunrRegisterPipelineFn({
+    //   cb: (token) => {
+    //     const $ = cheerioLoad(token);
+    //     return $.text();
+    //   }, lbl: "plaintextData"
+    // });
+    const documents = data.collections.posts.map(function (page) {
+      return {
+        url: page.url,
+        title: page.data.title,
+        date: page.date,
+        body: page.templateContent,
+        sections: splitHTMLIntoSections(page.templateContent)
+      };
     });
-    return sections
+    fs.writeFileSync(path.resolve(__dirname, "debug-lunr-docs.json"), JSON.stringify(documents, null, 2))
+
+    const idx = lunr(function () {
+      this.ref('id')
+      this.field('title')
+      this.field('body')
+
+      // Add the HTML tag removal function to the pipeline
+      // this.pipeline.add(removeHtmlTags);
+
+      documents.forEach(function (doc) {
+        this.add(doc)
+      }, this)
+    })
+
+    fs.writeFileSync(path.resolve(__dirname, "debug-lunr-index.json"), JSON.stringify(idx, null, 2))
+    return idx
   });
+  eleventyConfig.addJavaScriptFunction("useCheerioLoad", function (htmlContent) {
+    console.log("X", htmlContent, htmlContent.length)
+    return cheerio.load(htmlContent)
+  });
+  eleventyConfig.addJavaScriptFunction("lunrRegisterPipelineFn", function (data) {
+    const { cb, lbl } = data
+    return lunr.Pipeline.registerFunction(cb, lbl)
+  });
+
   // later in my .eleventy.js file...
   // https://keepinguptodate.com/pages/2019/06/creating-blog-with-eleventy/
   // function extractExcerpt(article) {
--- posts/search_lunr.11ty.js
+++ posts/search_lunr.11ty.js
@@ -7,29 +7,8 @@ class LunrIndex {
   }
 
   render(data) {
-    const documents = data.collections.posts.map(function (page) {
-      return {
-        url: page.url,
-        title: page.data.title,
-        date: page.date,
-        content: page.templateContent,
-        body: page.templateContent,
-      };
-    });
-    const lunr = this.useLunr //defined in .eleventy.js
-    const splitHTMLIntoSections = this.splitHTMLIntoSections //defined in .eleventy.js
-    console.log(splitHTMLIntoSections(`<h1 id="section-1">Section 1</h1><p>Lorem ipsum dolor sit amet.</p><h2 id="subsection-1-1">Subsection 1.1</h2><p>Consectetur adipiscing elit.</p><h2 id="subsection-1-2">Subsection 1.2</h2><p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><h1 id="section-2">Section 2</h1><p>Ut enim ad minim veniam.</p>';`))
-
-    const idx = lunr(function () {
-      this.ref('id')
-      this.field('title')
-      this.field('body')
-
-      documents.forEach(function (doc) {
-        this.add(doc)
-      }, this)
-    })
-
+    const useLunr = this.useLunr //defined in .eleventy.js
+    const idx = useLunr(data)
     return JSON.stringify(idx);
   }
 }
